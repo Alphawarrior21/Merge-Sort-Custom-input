e732c11f081d1ebda6110d3383b6974094a988ff
sumit chand <sumitchandmale@gmail.com>
2020-05-16 20:25:30 +0530

    Add project files.

---
+++ Merge Sort/MergeSort.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort
+{
+    class MergeSort
+    {
+       
+
+        public int[] Merge(int[] firstArray,int[] secondArray)
+        {
+            int finalArrayLength=firstArray.Length+secondArray.Length;
+            int[] result=new int[finalArrayLength];
+            int indexLeft=0,indexRight=0,indexResult=0;
+            while(indexLeft<firstArray.Length || indexRight<secondArray.Length)
+                {
+                   
+                 /**Case1 : When both the arrays have elements.**/
+                 if(indexLeft<firstArray.Length && indexRight<secondArray.Length)
+                    {
+                        /**Case : When item on left array is less then the item on the right array**/
+                        if(firstArray[indexLeft]<=secondArray[indexRight])
+                        {
+                           result[indexResult]=firstArray[indexLeft];
+                           indexLeft++;
+                           indexResult++;
+                        }
+                        else
+                        {
+                           result[indexResult]=secondArray[indexRight];
+                           indexRight++;
+                           indexResult++;
+                        }
+
+                    }
+                 else if(indexLeft<firstArray.Length)
+                    {
+                        result[indexResult]=firstArray[indexLeft];
+                           indexLeft++;
+                           indexResult++;
+                     
+                    }
+                 else if(indexRight<secondArray.Length)
+                    {
+                        result[indexResult]=secondArray[indexRight];
+                           indexRight++;
+                           indexResult++;
+                    }
+
+                }
+            return result;
+
+        }
+        public int[] MergeSortFunc(int[] inputArray)
+        {
+            int inputArraySize = inputArray.Length;
+            int[] leftArray;
+            int[] rightArray;
+            
+            if(inputArraySize<=1)
+                return inputArray;
+            //startIndex =inputArraySize>0 ? inputArray[0]:0;
+            int middleIndex=inputArraySize/2;
+
+            /**Prepare left array**/
+            leftArray=new int[middleIndex];
+
+            /**if array length is even number then the lenght of left array and right array will be same**/
+            if(inputArraySize%2==0)
+                rightArray=new int[middleIndex];
+            else
+                rightArray=new int[middleIndex+1];
+
+            /**Populate the left array**/
+            for(int i=0;i<middleIndex;i++)
+                leftArray[i]=inputArray[i];
+
+            /**populate right array**/
+            int x=0;
+            for(int i=middleIndex;i<inputArraySize;i++)
+                {
+                rightArray[x]=inputArray[i];
+                x++;
+                }
+                
+               
+            /**Recursively sort the left array**/
+            leftArray=MergeSortFunc(leftArray);
+            /**Recursively sort the right array**/
+            rightArray=MergeSortFunc(rightArray);
+            /**Merge the two sorted arrays**/
+            return Merge(leftArray,rightArray);
+
+        }
+    }
+}
---
+++ Merge Sort/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Merge_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            int inputArraySize;
+            int[] array;
+            MergeSort _mergeSort=new MergeSort();
+            int[] result;
+            Random random = new Random();
+            StringBuilder stringBuilder=new StringBuilder();
+            Console.WriteLine("Do you wish to entry array size manually (Input 1) or let the computer do it for you? (Input 2) ");
+            int optionselected= Convert.ToInt32(Console.ReadLine());
+            switch(optionselected)
+                { case 1 : Console.WriteLine("You have choosen to input size and element of the array manually");
+                    Console.WriteLine("Please enter size of the array");
+                    inputArraySize=Convert.ToInt32(Console.ReadLine());
+                    array=new int[inputArraySize];
+                     result =new int[inputArraySize];
+                     Console.WriteLine("Please enter elements of the array");
+                    for(int i = 0;i<inputArraySize;i++)
+                    {
+                       array[i]= Convert.ToInt32(Console.ReadLine());
+                       stringBuilder.Append(array[i]);
+                       stringBuilder.Append(" | ");
+                    }
+                    Console.WriteLine("Entered elements in the input array are :"); 
+                    Console.WriteLine(stringBuilder.ToString());
+                   
+                    Console.WriteLine("Performing merge sort"); 
+                    result= _mergeSort.MergeSortFunc(array);
+                    stringBuilder.Clear();
+                     Console.WriteLine("Sorting is done.Here is the sorted array"); 
+                    for(int i = 0;i<inputArraySize;i++)
+                    {
+                        stringBuilder.Append(result[i]);
+                        stringBuilder.Append(" | ");
+                    }
+                     Console.WriteLine(stringBuilder.ToString());
+ 
+                  break;
+                  case 2 :  Console.WriteLine("You have choosen to let computer prepare the input array for you and do the merge sort");
+                      
+                      
+                    Console.WriteLine("Selecting size of an array");
+                    inputArraySize=random.Next(10); 
+                    Console.WriteLine("Size selected : " + inputArraySize);
+                     array=new int[inputArraySize];
+                     result =new int[inputArraySize];
+                     Console.WriteLine("Entering elements of the array");
+                    for(int i = 0;i<inputArraySize;i++)
+                    {
+                       array[i]= random.Next(0,100);
+                       stringBuilder.Append(array[i]);
+                        stringBuilder.Append(" | ");
+                    }
+                    Console.WriteLine("Entered elements in the input array are :"); 
+                    Console.WriteLine(stringBuilder.ToString());
+                   
+                    Console.WriteLine("Performing merge sort"); 
+                    result= _mergeSort.MergeSortFunc(array);
+                    stringBuilder.Clear();
+                     Console.WriteLine("Sorting is done.Here is the sorted array"); 
+                     for(int i = 0;i<inputArraySize;i++)
+                    {
+                        stringBuilder.Append(result[i]);
+                        stringBuilder.Append(" | ");
+                    }
+                     Console.WriteLine(stringBuilder.ToString());
+                     
+                  break;
+                  default : Console.WriteLine("You havent choosen from the option available"); break;
+                }                  
+            Console.ReadLine();
+        }
+    }
+}
